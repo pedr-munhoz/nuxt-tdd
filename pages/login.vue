457d81bfd28e06e058f66aba78f8e4251261c331
Pedro Munhoz <pedr.munhoz@gmail.com>
2020-10-19 11:48:54 -0300

    LoginPage - early state tests.

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Login
+    <v-text-field id="emailInput" />
+    <v-text-field id="passwordInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+};
+</script>
+
+<style></style>
---
+++ test/pages/LoginPage.spec.js
@@ -0,0 +1,42 @@
+import { shallowMount } from '@vue/test-utils';
+import LoginPage from '@/pages/login';
+
+let wrapper = null;
+
+const title = 'Login';
+
+const build = () => {
+  const wrapper = shallowMount(LoginPage);
+
+  return {
+    wrapper,
+    emailField: () => wrapper.find('#emailInput'),
+    passwordField: () => wrapper.find('#passwordInput'),
+  };
+};
+
+beforeEach(() => {
+  ({ wrapper } = build());
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+describe('LoginPage', () => {
+  it('Contains a title', () => {
+    expect(wrapper.text()).toContain(title);
+  });
+
+  it('Contains a email text field', () => {
+    const { emailField } = build();
+
+    expect(emailField().exists()).toBe(true);
+  });
+
+  it('Contains a password text field', () => {
+    const { passwordField } = build();
+
+    expect(passwordField().exists()).toBe(true);
+  });
+});
